5b91dcb591f8c03309a305acf5b38d859b9f0b33
“Pavlova <anpavlova1997@gmail.com>
2020-09-10 07:41:19 +0300

    12

--- App/src/Api/printApi.php
+++ App/src/Api/printApi.php
@@ -1,2 +1,23 @@
 <?php
-$a = 'hello api';
+/*$client = new GuzzleHttp\Client();
+$response = $client->request('POST', 'https://stcorp.bitrix24.ru/rest/1/pmbrly0dp34srypl/profile/', [
+    'form_params' => [
+       'id' => '55555555'
+        // 'field_name' => 'abc',
+       // 'other_field' => '123',
+        //'nested_field' => [
+        //    'nested' => 'hello'
+       // ]
+    ]
+]);*/
+use GuzzleHttp\Client;
+
+$client = new GuzzleHttp\Client();
+$res = $client->request('GET', 'https://api.github.com/user', [
+    'auth' => ['user', 'pass']
+]);
+echo $res->getStatusCode();
+// "200"
+echo $res->getHeader('content-type')[0];
+// 'application/json; charset=utf8'
+echo $res->getBody();
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+use App\src\Api\printApi;
+require('App\src\Api\printApi.php');
+echo $a;
